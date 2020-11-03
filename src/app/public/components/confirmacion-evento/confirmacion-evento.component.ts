dfcd71a630f6b1148ae3879269b703a80c193cfb
= <fer.borgognoni@gmail.com>
2020-11-02 23:08:55 -0300

    Navegar a perfil de manera imaninaria

--- src/app/public/components/confirmacion-evento/confirmacion-evento.component.ts
+++ src/app/public/components/confirmacion-evento/confirmacion-evento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-confirmacion-evento',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmacionEventoComponent implements OnInit {
 
+  @Output() siguiente = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onVolver(): void {
+    this.siguiente.emit('nombre');
   }
 
 }
