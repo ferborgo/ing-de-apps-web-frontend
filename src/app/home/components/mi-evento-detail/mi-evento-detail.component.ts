49bf7150abcfe8ecaee46e7188b5d9ae5e9a4bd2
= <fer.borgognoni@gmail.com>
2021-01-26 23:51:43 -0300

    Se puede editar la config de un evento

--- src/app/home/components/mi-evento-detail/mi-evento-detail.component.ts
+++ src/app/home/components/mi-evento-detail/mi-evento-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { EventoControllerService } from 'destino';
+import { EventoControllerService, EventoPartial } from 'destino';
+import { DateUtils } from 'src/app/shared/utils/date.utils';
 
 @Component({
   selector: 'app-mi-evento-detail',
@@ -13,11 +14,26 @@ export class MiEventoDetailComponent implements OnInit {
   @Output() salir = new EventEmitter();
   urlInvitados: string;
 
+  eventoEdicion: EventoPartial;
+
   error: string;
 
-  constructor() { }
+  constructor(
+    private service: EventoControllerService
+  ) { }
 
   ngOnInit() {
+    console.log('evento: ', this.evento);
+    this.eventoEdicion = {
+      codigoResultados: this.evento.codigoResultados,
+      descripcion: this.evento.descripcion,
+      nombre: this.evento.nombre,
+      password: this.evento.password,
+      resultadosPublicos: this.evento.resultadosPublicos,
+      soloUnaOpcion: this.evento.soloUnaOpcion,
+      invitadosDinamicos: this.evento.invitadosDinamicos,
+      usuarioCreadorID: this.evento.usuarioCreadorID
+    };
     this.urlInvitados = this.armarURL();
   }
 
@@ -25,9 +41,22 @@ export class MiEventoDetailComponent implements OnInit {
     this.salir.emit(true);
   }
 
+  armarDate(opcion): string {
+    return DateUtils.generarTitulo(opcion.fechaInicio, opcion.fechaFinal);
+  }
+
   private armarURL(): string {
     const id = this.evento.id;
     return `localhost:4200/eventos/${id}`;
+  }
+
+  updateConfig(propiedad: string) {
+    this.eventoEdicion[propiedad] = ! this.eventoEdicion[propiedad];
+    this.service.eventoControllerUpdateById(this.evento.id, this.eventoEdicion).subscribe(
+      () => {
+        this.evento[propiedad] = this.eventoEdicion[propiedad];
+      }
+    );
   }
 
 }
